adb72808deba5fb80cfa3f5cd38a11a50de65ef5
Eric Patterson <Eric Patterson>
2017-06-19 21:48:40 -0600

    finished mentor test

--- toyProblems.js
+++ toyProblems.js
@@ -3,7 +3,7 @@
 module.exports = {
   reverseIt: (str) => {
     return str.split('').reverse().join('')
-  }
+  },
 
   //uncomment and finish the reverseIt function. It will take in one parameter which is a String and
   //reverse it
@@ -13,7 +13,7 @@ module.exports = {
     let isUnique = (num) => filtered.includes(num) ? null : filtered.push(num)
     arr.forEach( x => isUnique(x) )
     return filtered
-  }
+  },
 
   //uncomment and finish the removeDups function. It will take in one parameter which is an Array
   //remove all duplicates
@@ -22,7 +22,7 @@ module.exports = {
     let words = str.split(' ')
     let capFirst = (word) => word[0].toUpperCase() + word.substr(1)
     return words.map(word => capFirst(word)).join(' ')
-  }
+  },
 
   //uncomment and finish the titleIt function. It will take in one parameter which is a String and
   //capitalize the first letter of each word
@@ -33,7 +33,7 @@ module.exports = {
     let testVowel = (letter) => vowels.includes(letter.toLowerCase()) ? count ++ : null
     str.split('').map( letter => testVowel(letter) )
     return count
-  }
+  },
 
   //uncomment and finish the vowelCounter function. It will take in one parameter which is a String and
   //return the number of vowels in the string
@@ -45,7 +45,7 @@ module.exports = {
       }
     }
     return num > 1
-  }
+  },
 
   //uncomment and finish the isPrime function. It will take in one parameter which is a Number and
   //return true if it is prime and false if it is not
@@ -54,7 +54,7 @@ module.exports = {
   //var foo = 10 + '20';
   //uncomment the foo property and give your answer as its value
 
-  foo: '1020'
+  foo: '1020',
 
   //what is the outcome of the two console.logs below?
   //   var foo = "Hello";
@@ -65,6 +65,6 @@ module.exports = {
   // console.log(foo + bar);
   // uncomment the log1 and log2 properties and give your answers as their values
 
-  log1: "Hello World"
-  log2: "Hello"
+  log1: "Hello World",
+  log2: undefined
 }
